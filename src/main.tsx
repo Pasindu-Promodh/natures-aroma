62aef64752a97db35440d4ff8e0196a14714950f
Pasindu-Promodh <pasindugunathilaka96@gmail.com>
2025-10-08 20:08:11 +0530

    ada

--- src/main.tsx
+++ src/main.tsx
@@ -30,7 +30,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <I18nextProvider i18n={i18n}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <BrowserRouter>
+        <BrowserRouter basename="/nature-products">
           <App />
         </BrowserRouter>
       </ThemeProvider>
